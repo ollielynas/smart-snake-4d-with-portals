2e34b083300b11ade4103dec1f6d53c6e1abf2b6
ollielynas <lynasollie@gmail.com>
2022-11-23 13:11:48 +1300

    added dumb snake

--- src/main.rs
+++ src/main.rs
@@ -108,11 +108,27 @@ enum Direction {
 
 #[macroquad::main("Snake")]
 async fn main() {
-    fn coord(node: &Node, size: i32) -> (f32, f32) {
-        (
+    fn coord(node: &Node, size: i32, graph_type: i32) -> (f32, f32) {
+        let n = (node.x + node.y*size + node.z*size.pow(2) + node.w*size.pow(3)) as f32;
+        match graph_type {
+        0 => (
             15.0 * (node.x + (node.z) * size + (node.z + 1)) as f32,
             15.0 * (node.y + (node.w) * size + (node.w + 1)) as f32,
-        )
+        ),
+        1 => (
+            3.1415*n.powf(0.5)*n.powf(1.0).sin() * 10.0 + 3.141592 * (size as f32).powi(2) * 10.0,
+            3.1415*n.powf(0.5)*n.powf(1.0).cos() * 10.0 + 3.141592 * (size as f32).powi(2) * 10.0,
+        ),
+        2 => (
+            3.1415*n.powf(0.5)*n.powf(2.0).sin() * 10.0 + 1.0/3.141592 * (size as f32).powi(4),
+            3.1415*n.powf(0.5)*n.powf(2.0).cos() * 10.0 + 1.0/3.141592 * (size as f32).powi(4),
+        ),
+        3 => (
+            3.1415*n.powf(0.5)*n.powf(1.0).sin() * 10.0 + 6.0/3.141592 * (size as f32).powi(4),
+            3.1415*n.powf(0.5)*n.powf(2.0).cos() * 10.0 + 6.0/3.141592 * (size as f32).powi(4),
+        ),
+        _ => panic!("invalid graph type")
+        }
     }
     fn index_coord(n: f32) -> (f32, f32) {
         (
@@ -121,12 +137,23 @@ async fn main() {
         )
     }
 
+/*  .oooooo.                          .o88o.  o8o             
+ d8P'  `Y8b                         888 `"  `"'             
+888           .ooooo.  ooo. .oo.   o888oo  oooo   .oooooooo 
+888          d88' `88b `888P"Y88b   888    `888  888' `88b  
+888          888   888  888   888   888     888  888   888  
+`88b    ooo  888   888  888   888   888     888  `88bod8P'  
+-`Y8b0od8P'  `Y8bod8P' o888o o888o o888o   o888o `8oooooo.  
+-                                                d"     YD  
+-                                                "Y88888P'  
+*/
     let size = 6;
     let dimensions = 4;
     let portals = 10;
     let food = 10;
-
-
+    let graph_type = 0;
+    let user_control = false;
+    let snake_speed = 10;
 
 
     let mut b = match dimensions {
@@ -196,18 +223,18 @@ async fn main() {
             }
             _ => {}
         }
-        if now.elapsed().as_millis() > 500 {
+        if now.elapsed().as_millis() > snake_speed {
             now = Instant::now();
-            let head = &b.nodes[b.snake[b.snake.len()-1]];
-            println!("head {:?}", head);
-            let portaled = b.nodes[b.snake[b.snake.len()-1]].connections.len() == 1;
-            if portaled {
+
+            let mut head = b.nodes[b.snake[b.snake.len()-1]].clone();
+            let portaled = b.nodes[b.snake[b.snake.len()-1]].connections.len() == 1 || !user_control;
+            if portaled && user_control {
                 // portal_exit
                 b.snake.push(
                     b.nodes[b.snake[b.snake.len()-1]].connections[0]
                 );
             }
-            if match direction {
+            if match direction  {
                 Direction::X => {
                     if !portaled && (0 ..b.size ).contains(&((head.x  + speed))) {
                         b.snake.push(b.get_index(head.x + speed , head.y, head.z, head.w));
@@ -229,6 +256,43 @@ async fn main() {
                     }portaled || (0 ..b.size ).contains(&((head.w  + speed)))
                 },
             } {
+                if !user_control {
+    // --------------------------------- AI part start -----------------------------------------------//
+    let mut found_food = false;
+    for i in 0..head.connections.len() {
+        if b.nodes[head.connections[i]].is_food {
+            found_food = true;
+            b.snake.push(head.connections[i])
+        }
+    }
+    if !found_food {
+        let mut made_move = false;
+        head.connections.shuffle(&mut rng);
+        for l in head.connections {
+            if !b.nodes[l].is_snake {
+                made_move = true;
+                b.snake.push(l);
+                break
+            }
+        }
+        if !made_move {
+            println!("died");
+            direction = Direction::X;
+            speed = 1;
+            for i in &mut b.nodes {
+                i.is_snake = false;
+                i.is_food = false;
+                b.snake = vec![0,1];
+            }
+
+            for _ in 0..food {
+                b.nodes[rng.gen_range(0..l)].is_food = true;
+            }
+        }
+    }
+    //-------------------------------- AI part end --------------------------------------------------//
+    
+    }
                 // snake lived
                 if !b.nodes[b.snake[b.snake.len()-1]].is_food {
                     b.nodes[b.snake[0]].is_snake = false;
@@ -263,10 +327,10 @@ async fn main() {
     
     for i in &b.nodes {
             for j in &i.connections {
-                let a = (coord(i, size), coord(&b.nodes[*j], size));
+                let a = (coord(i, size, graph_type), coord(&b.nodes[*j], size, graph_type));
                 draw_line(a.0.0, a.0.1, a.1.0, a.1.1, 1.0, Color { r: 1.0, g: 1.0, b: 1.0, a: 0.5 })
             }
-            let p = coord(i, size);
+            let p = coord(i, size, graph_type);
             draw_circle(
                 p.0,
                 p.1,
@@ -292,7 +356,7 @@ async fn main() {
                 },
                 Color { r: 0.1, g: 0.1, b: 1.0, a: 1.0 }
                 );
-                let p2 = coord(&b.nodes[i.connections[0]], size);
+                let p2 = coord(&b.nodes[i.connections[0]], size, graph_type);
                 if i.is_snake {
                     draw_circle(
                     p2.0,
